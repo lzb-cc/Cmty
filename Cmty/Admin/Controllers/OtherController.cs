9ba29c574cb2d994337ef1678f152c2a31763c09
lzb-cc <937907891@qq.com>
2018-04-17 17:18:20 +0800

    something...

--- Cmty/Admin/Controllers/OtherController.cs
+++ Cmty/Admin/Controllers/OtherController.cs
@@ -3,15 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.Office.Interop.Excel;
 
 namespace Admin.Controllers
 {
-    public class OtherController : Controller
+    public class OtherController : AuthorityController
     {
+        private static Application application = new Application();
+        private Workbook workBook;
+        private const string url = "http://localhost:8090/";
+
         // GET: Other
         public ActionResult Index()
         {
             return View();
+        }
+
+        public void SetUserSets()
+        {
+            var ws = (Worksheet)workBook.Worksheets["UserSets"];
+            int rows = ws.UsedRange.Cells.Rows.Count;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                var model = new AccountService.RegisterView()
+                {
+                    Email = ((Range)ws.Cells[i, 1]).Text,
+                    Password = ((Range)ws.Cells[i, 2]).Text,
+                    UserName = ((Range)ws.Cells[i, 3]).Text,
+                    Tel = ((Range)ws.Cells[i, 4]).Text,
+                    University = Convert.ToInt32(((Range)ws.Cells[i, 5]).Text)
+                };
+                accountClient.Register(model);
+            }
+        }
+
+        public void SetTeacherSets()
+        {
+            var ws = (Worksheet)workBook.Worksheets["TeacherSets"];
+            int rows = ws.UsedRange.Cells.Rows.Count;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                var model = new TeacherService.TeacherInfoView()
+                {
+                    Email = ((Range)ws.Cells[i, 1]).Text,
+                    UserName = ((Range)ws.Cells[i, 2]).Text,
+                    RegisteDate = Convert.ToDateTime(((Range)ws.Cells[i, 3]).Text),
+                    Tel = ((Range)ws.Cells[i,4]).Text,
+                    University = Convert.ToInt32(((Range)ws.Cells[i, 5]).Text),
+                    Sex = ((Range)ws.Cells[i, 6]).Text,
+                    JobTitle = Convert.ToInt32(((Range)ws.Cells[i, 7]).Text),
+                    Desp = ((Range)ws.Cells[i, 8]).Text
+                };
+                teacherClient.AddTeacherInfo(model);
+            }
+        }
+
+        public ActionResult UploadData(string fileName)
+        {
+            workBook = application.Workbooks.Open(Url + fileName);
+            workBook.Close();
+            return RedirectToAction("Index");
         }
     }
 }
--- Cmty/UnitTest/Program.cs
+++ Cmty/UnitTest/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,10 +9,31 @@ namespace UnitTest
 {
     class Program
     {
+        public static void OpenExcel(string fileName)
+        {
+            var application = new Application();
+            var workBooks = application.Workbooks.Open(fileName);
+            var ws = (Worksheet)workBooks.Worksheets["TeacherSets"];
+            int rows = ws.UsedRange.Cells.Rows.Count;
+            int cols = ws.UsedRange.Columns.Count;
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= cols; j++)
+                {
+                    Console.WriteLine(((Range)ws.Cells[i, j]).Text);
+                }
+            }
+
+            workBooks.Close();
+        }
+
+
+
+
         static void Main(string[] args)
         {
-            var client = new ServiceReference1.AccountServiceClient();
-            Console.WriteLine(client.Register(null));
+            OpenExcel("http://localhost:8090/tb_datas.xlsx");
         }
     }
 }
