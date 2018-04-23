cd3970c8f6b2b4841aa25e213772ce5538efe2c2
lzb-cc <937907891@qq.com>
2018-04-23 20:23:41 +0800

    ...

--- Cmty/Services/cnts/IForumService.cs
+++ Cmty/Services/cnts/IForumService.cs
@@ -2,13 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Services.cnts
 {
+    [ServiceContract]
     interface IForumService
     {
+        [OperationContract]
+        CommonLib.ReturnState AddPost(PostModel model);
+
+        [OperationContract]
+        CommonLib.ReturnState DelPost(int id);
+
+        [OperationContract]
+        List<PostModel> GetPostList();
     }
 
     [DataContract]
